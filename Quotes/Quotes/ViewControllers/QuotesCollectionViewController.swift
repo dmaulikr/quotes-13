3c53de926ab3ffef25ce95b5d2259502e8c4066c
Jaime Paulo <jaime.azevedo.88@gmail.com>
2016-09-11 15:54:35 +0100

    Creating app screen architecture.

---
+++ Quotes/Quotes/ViewControllers/QuotesCollectionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  QuotesCollectionViewController.swift
+//  Quotes
+//
+//  Created by Jaime Paulo on 11/09/16.
+//  Copyright © 2016 Doist. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class QuotesCollectionViewController: UICollectionViewController {
+
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Private API
+    private dynamic func addNewQuote() {
+
+    }
+}
